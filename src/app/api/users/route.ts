d0e0a8015d09a9dc15d740facbf5b8b569773853
Behzad <bh.ahmady@gmail.com>
2025-10-08 12:12:45 +0330

    refactor(api): replace direct PrismaClient with shared instance
    Use shared prisma instance from lib to avoid multiple connections and improve performance

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest } from 'next/server'
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '@/src/lib/prisma'
 import bcrypt from 'bcryptjs'
-const prisma = new PrismaClient()
 
 export const runtime = 'nodejs'
 
